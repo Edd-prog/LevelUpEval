db2a9cc336b32e2533a23b827cd01afe65a13cfe
Edd-prog <71219278+Edd-prog@users.noreply.github.com>
2023-09-30 22:04:55 +0100

    styling

--- levelup_client/src/components/CardForm.vue
+++ levelup_client/src/components/CardForm.vue
@@ -7,12 +7,23 @@ const toast = inject('toast')
 
 const cardInfo = ref({
   type: 'visa',
+  username: null,
+  cvv: null,
+  pan: null,
+  month: null,
+  year: null
+})
+
+/*
+const cardInfo = ref({
+  type: 'visa',
   username: 'Edgar Andrade',
   cvv: 123,
   pan: 38345688888888888,
   month: 8,
   year: 2023
 })
+*/
 
 const sendOrderToAPI = async () => {
   console.log(cardInfo.value)
@@ -24,26 +35,29 @@ const sendOrderToAPI = async () => {
     })
     .catch((error) => {
       console.log(error)
-      toast.error('Card info is invalid')
+      toast.error(error.response.data)
     })
 }
 </script>
 
 <template>
   <div class="container">
-    <div class="card bg-primary text-white rounded-3">
+    <div class="credit-card">
       <div class="card-body">
-        <h1 class="small mb-2">Credit Card</h1>
+        <h1 class="mb-2">Credit Card</h1>
 
         <form class="mt-4">
           <div class="form-outline form-white mb-4">
-            <label> Card type</label>
-            <select v-model="cardInfo.type">
-              <option value="visa">VISA</option>
-              <option value="am">American Express</option>
-              <option value="mc">Mastercard</option>
-            </select>
+            <div class="d-flex flex-row gap-3">
+              <label> Card type</label>
+              <select class="input-group-text" v-model="cardInfo.type">
+                <option value="visa">VISA</option>
+                <option value="am">American Express</option>
+                <option value="mc">Mastercard</option>
+              </select>
+            </div>
           </div>
+
           <div class="form-outline form-white mb-4">
             <label> Card number</label>
             <input
@@ -52,23 +66,47 @@ const sendOrderToAPI = async () => {
               class="form-control form-control-lg"
               minlength="16"
               maxlength="19"
+              placeholder="4321123548786999"
             />
           </div>
 
           <div class="form-outline form-white mb-4">
             <label> Card holder</label>
-            <input v-model="cardInfo.username" type="text" class="form-control form-control-lg" />
+            <input
+              v-model="cardInfo.username"
+              type="text"
+              class="form-control form-control-lg"
+              placeholder="John Smith"
+            />
           </div>
 
           <div class="form-outline form-white mb-4">
             <label> Expiration Date</label>
-            <input v-model="cardInfo.month" type="text" class="form-control form-control-lg" />
-            <input v-model="cardInfo.year" type="text" class="form-control form-control-lg" />
+            <div class="d-flex flex-row gap-2">
+              <input
+                v-model="cardInfo.month"
+                type="text"
+                class="form-control form-control-lg"
+                placeholder="Month"
+              />
+
+              <input
+                v-model="cardInfo.year"
+                type="text"
+                class="form-control form-control-lg"
+                placeholder="Year"
+              />
+            </div>
           </div>
 
           <div class="form-outline form-white mb-4">
             <label> CVV</label>
-            <input v-model="cardInfo.cvv" type="text" class="form-control form-control-lg" />
+            <input
+              v-model="cardInfo.cvv"
+              type="text"
+              class="form-control form-control-lg"
+              placeholder="123"
+            />
           </div>
         </form>
 
@@ -85,3 +123,16 @@ const sendOrderToAPI = async () => {
     </div>
   </div>
 </template>
+
+<style scoped>
+.credit-card {
+  background: linear-gradient(to right, #6495ed, #4169e1);
+  color: white;
+  border-radius: 10px;
+  padding: 20px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  font-family: Arial, sans-serif;
+}
+
+/* Style your form fields and other elements as needed */
+</style>
--- levelup_client/src/main.js
+++ levelup_client/src/main.js
@@ -3,8 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+//import Vue from 'vue'
 import Toaster from '@meforma/vue-toaster'
+//import { BootstrapVue } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 const app = createApp(App)
 
@@ -16,6 +20,8 @@ app.use(Toaster, {
   timeout: 3000,
   pauseOnHover: true
 })
+
+//Vue.use(BootstrapVue)
 
 app.provide('toast', app.config.globalProperties.$toast)
 
