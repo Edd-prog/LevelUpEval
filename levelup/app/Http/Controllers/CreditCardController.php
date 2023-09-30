3c9c349c72244d9ab744348d4495de45350cd267
Edd-prog <71219278+Edd-prog@users.noreply.github.com>
2023-09-30 18:31:17 +0100

    added comments

--- levelup/app/Http/Controllers/CreditCardController.php
+++ levelup/app/Http/Controllers/CreditCardController.php
@@ -3,21 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CredidCardPost;
-use Illuminate\Http\Request;
 
 class CreditCardController extends Controller
 {
     public function verification(CredidCardPost $request)
     {
-        //dd($request);
+        //validates data
         $cardInfo = $request->validated();
 
-
+        //validates card and type and cvv
         $cvvLen = strlen((string)$cardInfo['cvv']);
         if (($cardInfo['type'] == 'visa' || $cardInfo['type'] == 'mc') && $cvvLen != 3) {
 
             return response('cvv incorrect', 422)->header('Content-Type', 'application/json');
         }
+        //validates pan for american express
         if ($cardInfo['type'] == 'am') {
 
             if ($cvvLen != 4) {
@@ -30,27 +30,12 @@ class CreditCardController extends Controller
                 return response('incorrect pan', 422)->header('Content-Type', 'application/json');
             }
         }
-
-        //dd(intval(date('m')));
+        //validates experiration date of card
         if ($cardInfo['year'] == date('Y') && $cardInfo['month'] < intval(date('m'))) {
             return response('card expired', 422)->header('Content-Type', 'application/json');
         }
 
 
-
         return response('card info correct', 200)->header('Content-Type', 'application/json');
     }
-
-
-    /*
-        The expiry date of the credit card (year and month) must be AFTER present time
-        The CVV (security code) of the credit card must be exactly 3 digits long
-        Unless it’s an American Express card, in which case the CVV must be exactly 4 digits long
-       
-        American Express are cards whose PAN (card numbers) starts with either “34” or “37”
-
-
-        The PAN (card number) is between 16 and 19 digits long
-        Last digit of the PAN (card number) is checked using Luhn’s algorithm
-    */
 }
