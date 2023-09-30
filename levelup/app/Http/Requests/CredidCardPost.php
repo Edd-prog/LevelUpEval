f07e6dcb17eb258db512645ad0443e76efc22c88
Edd-prog <71219278+Edd-prog@users.noreply.github.com>
2023-09-30 17:22:18 +0100

    Post add date rules

--- levelup/app/Http/Requests/CredidCardPost.php
+++ levelup/app/Http/Requests/CredidCardPost.php
@@ -22,11 +22,15 @@ class CredidCardPost extends FormRequest
     public function rules(): array
     {
         return [
-            //date
-            'type'      => 'required|string|in:visa,am,mc',
-            'username'  => 'required|string|max:50',
-            'cvv'   =>  'required|integer|digits_between:3,4',
-            'pan'   =>  'required|integer|digits_between:16,19',
+            'type'      =>  'required|string|in:visa,am,mc',
+            'username'  =>  'required|string|max:50',
+            'cvv'       =>  'required|integer|digits_between:3,4',
+            'pan'       =>  'required|integer|digits_between:16,19',
+            'month'     =>  'required|integer|between:1,12',
+            'year'      =>  'required|digits:4|integer|min:' . date('Y'),
+
+
+            //year' => 'required|digits:4|integer|min:1900|max:'.(date('Y')+1),
         ];
     }
     public function messages()
